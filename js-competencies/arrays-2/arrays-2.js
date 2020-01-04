6f66bf7326788f1e83f88c39bc4b55a49f8102be
Zachary Johnson <zach_j@att.net>
2020-01-04 13:09:54 -0700

    in arr 2

--- js-competencies/arrays-2/arrays-2.js
+++ js-competencies/arrays-2/arrays-2.js
@@ -9,7 +9,12 @@
 //3rd element of the array with the string '42 ducks'. 
 //addMoreDucks should return the array.
 
-//Code here
+function addMoreDucks(arr){
+    if(arr.length > 4){
+        arr.splice(2,1,'42 ducks')
+    }
+    return arr
+}
 
 
 //////////////////PROBLEM 2////////////////////
@@ -18,7 +23,12 @@
 //and if the array's length is greater than 4, removes 
 //the 3rd element of the array. cutItOut should return the array.
 
-//Code here
+function cutItOut(arr){
+    if(arr.length > 4){
+        arr.splice(2,1)
+    }
+    return arr
+}
 
 
 //////////////////PROBLEM 3////////////////////
@@ -27,5 +37,12 @@
 //and finds the element "Tolkn", and changes it to "Tolkien".
 //changeName should return the array.
 
-//Code here
+function changeName(arr){
+    for(let i = 0; i < arr.length - 1; i++){
+        if(arr[i] === 'Tolkn'){
+            arr.splice(i,1,'Tolkien')
+        }
+    }
+    return arr
+}
 
